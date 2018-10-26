f182b948cc4dd14e4d3bdb6c9130fb62d6c3af14
Michael Andrews <mandrews@github.com>
2018-10-26 15:56:49 -0700

    Add types to handler signature

--- docker-sam/src/index.ts
+++ docker-sam/src/index.ts
@@ -1,7 +1,10 @@
 import * as xml2json from 'xml2json';
+import { Callback, Context, Handler } from 'aws-lambda';
 
-exports.handler = async (event, context, callback) => {
+const handler: Handler = (event: any, context: Context, callback: Callback): void => {
   console.log("received event: %j", event);
   const xml = xml2json.toXml(event);
   callback(null, xml);
 };
+
+export { handler  }
